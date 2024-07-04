0f1b7f3431ffd35982d04c74d00d8500f28827d1
Luciano Amato <luciano2697@outlook.com.ar>
2024-07-04 13:14:19 -0300

    Inicio style

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,10 +1,9 @@
 import { Link } from "react-router-dom";
-import escudo from '/images/escudo.webp'
 
 const Aside = () => {
-  const buttonStyle = "p-2 border-white border-y drop-shadow-xl bg-blue-500"
+  const buttonStyle = "p-2 border-black border-y drop-shadow-xl bg-slate-500 text-black italic font-medium"
   return (
-    <aside className="h-full w-2/12 flex flex-col justify-center py-4 bg-blue-900 text-white">
+    <aside className="h-4/5 border-r border-black w-40 flex flex-col justify-center bg-blue-900">
       {/* <img src={escudo} alt="escudo" className="w-1/3 self-center top-4 right-4" /> */}
       <ul className="flex flex-col justify-around h-full text-center">
         <li className={buttonStyle}>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
+import escudo from '/images/escudo.webp'
+
 const Header = () => {
     return(
-        <div className="bg-blue-500 h-1/2">
-            
+        <div className="bg-blue-500 h-max border-b border-black">
+            <div className='w-40 flex justify-center border-r border-black'>
+                <img src={escudo} alt="escudo" className="w-20 m-2" />
+            </div>
+            <div></div>
+            <div></div>
         </div>
     )
 }
 
 export default Header
 
-// import escudo from '/images/escudo.webp'
 
-//{/* <img src={escudo} alt="escudo" className="w-1/3 self-center top-4 right-4" /> */}
