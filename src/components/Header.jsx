1f99a54797fa6fe1a46b67620fa3a44fd65e225e
LucianoAmato <luciano2697@outlook.com.ar>
2024-07-17 23:43:12 -0300

    Header  styles v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             <div className='w-40 flex justify-center'>
                 <img src={escudo} alt="escudo" className="w-20 m-2" />
             </div>
-            <div className="bg-slate-800 self-center mb-2 rounded-lg py-9 px-4 w-2/3 flex-grow mr-5">
+            <div className="bg-slate-800 self-center mb-2 rounded-lg py-9 px-4 w-2/3 flex-grow mr-2">
                 <p className='text-white text-4xl tracking-wide text-center'>Distrito Electoral {"-------------".toUpperCase()}</p>
             </div>
             <Logout/>
--- src/components/Logout.jsx
+++ src/components/Logout.jsx
@@ -4,12 +4,12 @@ import { FaUser } from "react-icons/fa";
 
 const Logout = () => {
     return(
-        <div className="flex flex-row justify-center top-0 right-0 text-lg mr-2 mt-2 h-7 border border-black items-center shadow-xl">
-            <div className="flex flex-row items-center bg-slate-200 px-1 mr-1 rounded-sm">
+        <div className="flex flex-row justify-center top-0 right-0 text-lg mr-2 mt-3 h-7 border border-black items-center shadow-xl">
+            <div className="flex flex-row items-center bg-slate-100 px-1 mr-1 rounded-sm">
                 <FaUser className="mr-2"/>
-                <p>Apellido, Nombre</p>
+                <p>{"Apellido"}, {"Nombre"}</p>
             </div>
-            <Link to={''}>
+            <Link to={'/'}>
                 <HiOutlineLogout className="rounded-sm text-3xl bg-red-500 border-y border-black hover:bg-red-400 duration-100" title="cerrar sesion"/>
             </Link>
         </div>
